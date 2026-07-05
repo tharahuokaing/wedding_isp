426a3800c82667dc04f1f6fa1c2eea102e8138e7
THARA HUOKAING <tharahuokaing@gmail.com>
2026-07-05 23:28:01 +0700

    Change wedding audio source file to huokaingthara.mp3

--- script1.js
+++ script1.js
@@ -6,7 +6,7 @@
  * Audio Engine Configuration
  * Uses the local wedding audio asset path
  */
-const weddingAudioSrc = "soma.mp3";
+const weddingAudioSrc = "huokaingthara.mp3";
 
 // Create HTMLAudioElement instance
 const weddingAudio = new Audio(weddingAudioSrc);
